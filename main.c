17cdf2663c9f0358e04d24124fbea6c7a38affad
nicekwell <nicekwell@gmail.com>
2017-01-03 20:42:09 +0800

    添加速度控制功能。

--- main.c
+++ main.c
@@ -13,6 +13,18 @@ sbit key_step_m1=P1^1;
 sbit key_step_p1=P3^4;
 sbit key_stop=P1^2;
 
+unsigned char f = 0;	//切换频率，一秒钟自动切换多少次
+unsigned char T;	//周期，每隔多长时间切换一次，和f对应，单位是5ms
+void setf(unsigned char f)
+{
+    double T_temp;
+    if(f != 0)
+    {
+	T_temp = (1/(double)f) * 200;
+	T = (unsigned char)T_temp;
+    }
+}
+
 unsigned char code list[6][6] = {
     {0, 1, 1, 0, 0, 0},	//v-->u
     {0, 0, 1, 0, 0, 1},	//v-->w
@@ -57,22 +69,61 @@ void delay5(unsigned char n)
 
 void main()
 {
-    P0=0;
+    step_forward();
+    step_forward();
+    f = 0;
+    setf(f);
     while(1)
     {
 	delay5(1);
 	{
-	    static key_step_m1_last=1, key_step_p1_last=1;
+	    static key_step_m1_last=1, key_step_p1_last=1, key_speed_m1_last, key_speed_p1_last, key_stop_last;;
 	    if(key_step_m1_last == 1 && key_step_m1 ==0)
 	    {
 		step_back();
 	    }
-	    if(key_step_p1_last == 1 && key_step_p1 == 0)
+	    else if(key_step_p1_last == 1 && key_step_p1 == 0)
 	    {
 		step_forward();
 	    }
+	    else if(key_speed_m1_last == 1 && key_speed_m1 == 0)
+	    {
+		if(f != 0)
+		{
+		    f--;
+		    setf(f);
+		}
+	    }
+	    else if(key_speed_p1_last == 1 && key_speed_p1 == 0)
+	    {
+		if(f != 200)
+		{
+		    f++;
+		    setf(f);
+		}
+	    }
+	    else if(key_stop_last == 1 && key_stop == 0)
+	    {
+		f=0;
+		setf(f);
+	    }
 	    key_step_m1_last=key_step_m1;
 	    key_step_p1_last=key_step_p1;
+	    key_speed_m1_last = key_speed_m1;
+	    key_speed_p1_last = key_speed_p1;
+	    key_stop_last = key_stop;
+	}
+	{	//设置速度控制进程
+	    static unsigned char num=0;
+	    if(f)
+	    {
+		num++;
+		if(num >= T)
+		{
+		    num = 0;
+		    step_forward();
+		}
+	    }
 	}
     }
 }
